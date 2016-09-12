b6bedc54e76cc9de0f8c716c7c6bc2b59596e533
Gustavo Guimaraes <gustavo.ceci95@gmail.com>
2016-09-12 13:49:02 -0300

    Added comunication with external API (swapi) for testing

--- SampleClient/main.js
+++ SampleClient/main.js
@@ -1,17 +1,31 @@
-var mqtt    = require('mqtt');
+var request = require('request');
+var mqtt = require('mqtt');
+
+var API_URL = "https://swapi.co/api/";
+
 var pub  = mqtt.connect('mqtt://test.mosquitto.org');
 var sub  = mqtt.connect('mqtt://test.mosquitto.org');
 
+// Subscriber setup
 sub.on('connect', function () {
-  sub.subscribe('presence');
-});
-
-pub.on('connect', function () {
-  pub.publish('presence', 'Hello mqtt');
+  sub.subscribe('swapi');
+  console.log("Subscribed to swapi");
 });
 
 sub.on('message', function (topic, message) {
   // message is Buffer
-  console.log(message.toString());
+  console.log("Received Message on swapi");
+  var apiResult = request.get(API_URL + message.toString(), function(error, response, body) {
+    console.log(body);
+  });
   sub.end();
+});
+
+// Publisher setup
+pub.on('connect', function () {
+  setInterval(() => {
+    pub.publish('swapi', 'people/1');
+    console.log("Published to swapi");
+  }, 1000);
+
 });
